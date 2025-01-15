c5a1aaa153913c8df91001ff9d222ab98335b190
christopher <108479484+ChristopherWilsar@users.noreply.github.com>
2025-01-15 23:10:54 +0700

    feat(Vercel): add deploying to vercel json; change conneciton method

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 
 const hrisPool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
 })
 
 module.exports = {
